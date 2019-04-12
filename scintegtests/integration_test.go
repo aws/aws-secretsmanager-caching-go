c788a24b0ef58a75603d6e98965028126a70cdbb
Parimal Deshmukh <dparimal@amazon.com>
2019-04-12 00:02:16 +0000

    Hardening of prev run secret deletion logic

--- scintegtests/integration_test.go
+++ scintegtests/integration_test.go
@@ -13,8 +13,8 @@ import (
 )
 
 var (
-	randStringSet   = []rune("abcdefghijklmnopqrstuvwxyz0123456789")
-	subTests        = []func(t *testing.T, api secretsmanageriface.SecretsManagerAPI) string{
+	randStringSet = []rune("abcdefghijklmnopqrstuvwxyz0123456789")
+	subTests      = []func(t *testing.T, api secretsmanageriface.SecretsManagerAPI) string{
 		integTest_getSecretBinary,
 		integTest_getSecretBinaryWithStage,
 		integTest_getSecretString,
@@ -37,7 +37,7 @@ func generateRandString(n int) string {
 
 func generateSecretName(testName string) (string, string) {
 	clientRequestToken := generateRandString(32)
-	secretName := testName + "-" + clientRequestToken
+	secretName := "scIntegTest_" + testName + "_" + clientRequestToken
 
 	return secretName, clientRequestToken
 }
@@ -55,6 +55,66 @@ func createSecret(
 	return api.CreateSecret(createSecretInput)
 }
 
+// Lazily delete all the secrets we created
+// Also delete secrets created over 2 days ago, with the "scIntegTest_" prefix
+func cleanupSecrets(secretNames *[]string, secretsManagerClient *secretsmanager.SecretsManager, t *testing.T) {
+
+	// Cleanup secrets created on this test run
+	performDelete(secretNames, secretsManagerClient, true)
+
+	prevRunSecrets := getPrevRunSecrets(secretsManagerClient)
+	for _, secretARN := range prevRunSecrets {
+		t.Logf("Scheduling deletion for secret: \"%s\"", secretARN)
+	}
+
+	// Cleanup secrets created on past runs
+	performDelete(&prevRunSecrets, secretsManagerClient, false)
+}
+
+func getPrevRunSecrets(secretsManagerClient *secretsmanager.SecretsManager) []string {
+	var nextToken *string
+	var secretNames []string
+	twoDaysAgo := time.Now().Add(-(48 * time.Hour))
+	testSecretNamePrefix := "^scIntegTest_.+"
+
+	for {
+		resp, err := secretsManagerClient.ListSecrets(
+			&secretsmanager.ListSecretsInput{NextToken: nextToken},
+		)
+
+		if resp == nil || err != nil {
+			break
+		}
+
+		for _, secret := range resp.SecretList {
+			var name []byte
+			copy(name, *secret.Name)
+			match, _ := regexp.Match(testSecretNamePrefix, name)
+			if match && secret.LastChangedDate.Before(twoDaysAgo) && secret.LastAccessedDate.Before(twoDaysAgo) {
+				secretNames = append(secretNames, *secret.ARN)
+			}
+		}
+
+		if resp.NextToken == nil {
+			break
+		}
+
+		nextToken = resp.NextToken
+		time.Sleep(1 * time.Second)
+	}
+	return secretNames
+}
+
+func performDelete(secretNames *[]string, secretsManagerClient *secretsmanager.SecretsManager, forceDelete bool) {
+	for _, secretName := range *secretNames {
+		time.Sleep(time.Second / 2)
+		_, _ = secretsManagerClient.DeleteSecret(&secretsmanager.DeleteSecretInput{
+			SecretId:                   &secretName,
+			ForceDeleteWithoutRecovery: &forceDelete,
+		})
+	}
+}
+
 func TestIntegration(t *testing.T) {
 
 	// Create a new API client
@@ -69,55 +129,11 @@ func TestIntegration(t *testing.T) {
 	var secretNames []string
 
 	// Defer cleanup of secrets to ensure cleanup in case of caller function being terminated
-	defer cleanupSecrets(&secretNames, secretsManagerClient)
+	defer cleanupSecrets(&secretNames, secretsManagerClient, t)
 
 	// Run integ tests
 	for _, testFunc := range subTests {
 		secretNames = append(secretNames, testFunc(t, secretsManagerClient))
-	}
-}
-
-// Lazily delete all the secrets we created
-// Also delete secrets created over 2 days ago, with the "integTest" prefix
-func cleanupSecrets(secretNames *[]string, secretsManagerClient *secretsmanager.SecretsManager) {
-	var true = true
-	var nextToken *string
-	twoDaysAgo := time.Now().Add(- (48 * time.Hour))
-	testSecretNamePrefix := "^integTest_.+"
-
-	for {
-		resp, err := secretsManagerClient.ListSecrets(
-				&secretsmanager.ListSecretsInput{NextToken: nextToken},
-			)
-
-		if resp == nil || err != nil {
-			break
-		}
-
-		for _, secret := range resp.SecretList {
-			var name []byte
-			copy(name, *secret.Name)
-			match, _ := regexp.Match(testSecretNamePrefix, name)
-			if match && secret.LastChangedDate.Before(twoDaysAgo) && secret.LastAccessedDate.Before(twoDaysAgo) {
-				*secretNames = append(*secretNames, *secret.Name)
-			}
-		}
-
-		if resp.NextToken == nil {
-			break
-		}
-
-		nextToken = resp.NextToken
-		time.Sleep(1 * time.Second)
-	}
-
-
-	for _, secretName := range *secretNames {
-		time.Sleep(time.Second / 2)
-		_, _ = secretsManagerClient.DeleteSecret(&secretsmanager.DeleteSecretInput{
-			SecretId:                   &secretName,
-			ForceDeleteWithoutRecovery: &true,
-		})
 	}
 }
 
@@ -127,10 +143,10 @@ func integTest_getSecretBinary(t *testing.T, api secretsmanageriface.SecretsMana
 	)
 
 	secretBinary := []byte{0, 1, 1, 0, 0, 1, 1, 0}
-	createResult, err := createSecret("integTest_getSecretBinary", nil, secretBinary, api)
+	createResult, err := createSecret("getSecretBinary", nil, secretBinary, api)
 
 	if err != nil {
-		t.Errorf("Failed to create secret \"integTest_getSecretBinary\" ERROR: %s", err)
+		t.Errorf("Failed to create secret \"getSecretBinary\" ERROR: %s", err)
 		return ""
 	}
 
@@ -154,10 +170,10 @@ func integTest_getSecretBinaryWithStage(t *testing.T, api secretsmanageriface.Se
 	)
 
 	secretBinary := []byte{0, 1, 1, 0, 0, 1, 1, 0}
-	createResult, err := createSecret("integTest_getSecretBinaryWithStage", nil, secretBinary, api)
+	createResult, err := createSecret("getSecretBinaryWithStage", nil, secretBinary, api)
 
 	if err != nil {
-		t.Errorf("Failed to create secret \"integTest_getSecretBinaryWithStage\" ERROR: %s", err)
+		t.Errorf("Failed to create secret \"getSecretBinaryWithStage\" ERROR: %s", err)
 		return ""
 	}
 
@@ -204,10 +220,10 @@ func integTest_getSecretString(t *testing.T, api secretsmanageriface.SecretsMana
 		func(c *secretcache.Cache) { c.Client = api },
 	)
 	secretString := "This is a secret"
-	createResult, err := createSecret("test_getSecretString", &secretString, nil, api)
+	createResult, err := createSecret("getSecretString", &secretString, nil, api)
 
 	if err != nil {
-		t.Errorf("Failed to create secret: \"test_getSecretString\" ERROR: %s", err)
+		t.Errorf("Failed to create secret: \"getSecretString\" ERROR: %s", err)
 		return ""
 	}
 
@@ -231,7 +247,7 @@ func integTest_getSecretStringWithStage(t *testing.T, api secretsmanageriface.Se
 	)
 
 	secretString := "This is a secret string"
-	createResult, err := createSecret("integTest_getSecretStringWithStage", &secretString, nil, api)
+	createResult, err := createSecret("getSecretStringWithStage", &secretString, nil, api)
 
 	if err != nil {
 		t.Error("Failed to create secret ", err)
@@ -284,10 +300,10 @@ func integTest_getSecretStringWithTTL(t *testing.T, api secretsmanageriface.Secr
 	)
 
 	secretString := "This is a secret"
-	createResult, err := createSecret("integTest_getSecretStringWithTTL", &secretString, nil, api)
+	createResult, err := createSecret("getSecretStringWithTTL", &secretString, nil, api)
 
 	if err != nil {
-		t.Errorf("Failed to create secret: \"test_getSecretString\" ERROR: %s", err)
+		t.Errorf("Failed to create secret: \"getSecretString\" ERROR: %s", err)
 		return ""
 	}
 
