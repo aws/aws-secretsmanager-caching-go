9baae96da80a27a0f08188d8843a4c08b233f237
wlewis4321 <58235577+wlewis4321@users.noreply.github.com>
2024-07-08 10:04:20 -0500

    Upgrading AWS Go SDK to v2 (#80)
    Co-authored-by: William Lewis <58235577+wlewis4321@users.noreply.github.com>
        Co-authored-by: megaproaktiv <silberkopf@gmail.com>

--- secretcache/mockClient_test.go
+++ secretcache/mockClient_test.go
@@ -14,17 +14,17 @@
 package secretcache_test
 
 import (
+	"context"
 	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/request"
-	"github.com/aws/aws-sdk-go/service/secretsmanager"
-	"github.com/aws/aws-sdk-go/service/secretsmanager/secretsmanageriface"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
+	"github.com/aws/aws-secretsmanager-caching-go/secretcache"
 )
 
 // A struct to be used in unit tests as a mock Client
 type mockSecretsManagerClient struct {
-	secretsmanageriface.SecretsManagerAPI
+	secretcache.SecretsManagerAPIClient
 	MockedGetResult         *secretsmanager.GetSecretValueOutput
 	MockedDescribeResult    *secretsmanager.DescribeSecretOutput
 	GetSecretValueErr       error
@@ -38,9 +38,9 @@ func newMockedClientWithDummyResults() (mockSecretsManagerClient, string, string
 	createDate := time.Now().Add(-time.Hour * 12) // 12 hours ago
 	versionId := getStrPtr("very-random-uuid")
 	otherVersionId := getStrPtr("other-random-uuid")
-	versionStages := []*string{getStrPtr("hello"), getStrPtr("versionStage-42"), getStrPtr("AWSCURRENT")}
-	otherVersionStages := []*string{getStrPtr("AWSPREVIOUS")}
-	versionIdsToStages := make(map[string][]*string)
+	versionStages := []string{"hello", "versionStage-42", "AWSCURRENT"}
+	otherVersionStages := []string{"AWSPREVIOUS"}
+	versionIdsToStages := make(map[string][]string)
 	versionIdsToStages[*versionId] = versionStages
 	versionIdsToStages[*otherVersionId] = otherVersionStages
 	secretId := getStrPtr("dummy-secret-name")
@@ -69,7 +69,7 @@ func newMockedClientWithDummyResults() (mockSecretsManagerClient, string, string
 }
 
 // Overrides the interface method to return dummy result.
-func (m *mockSecretsManagerClient) GetSecretValueWithContext(context aws.Context, input *secretsmanager.GetSecretValueInput, opts ...request.Option) (*secretsmanager.GetSecretValueOutput, error) {
+func (m *mockSecretsManagerClient) GetSecretValue(context context.Context, input *secretsmanager.GetSecretValueInput, optFns ...func(*secretsmanager.Options)) (*secretsmanager.GetSecretValueOutput, error) {
 	m.GetSecretValueCallCount++
 
 	if m.GetSecretValueErr != nil {
@@ -80,7 +80,7 @@ func (m *mockSecretsManagerClient) GetSecretValueWithContext(context aws.Context
 }
 
 // Overrides the interface method to return dummy result.
-func (m *mockSecretsManagerClient) DescribeSecretWithContext(context aws.Context, input *secretsmanager.DescribeSecretInput, opts ...request.Option) (*secretsmanager.DescribeSecretOutput, error) {
+func (m *mockSecretsManagerClient) DescribeSecret(context context.Context, input *secretsmanager.DescribeSecretInput, optFns ...func(*secretsmanager.Options)) (*secretsmanager.DescribeSecretOutput, error) {
 	m.DescribeSecretCallCount++
 
 	if m.DescribeSecretErr != nil {
@@ -92,5 +92,5 @@ func (m *mockSecretsManagerClient) DescribeSecretWithContext(context aws.Context
 
 // Helper function to get a string pointer for input string.
 func getStrPtr(str string) *string {
-	return &str
+	return aws.String(str)
 }
